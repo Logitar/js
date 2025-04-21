56df34a9ee3b4a14a74791519fc665b046d1d66d
Francis Pion <francispion@hotmail.com>
2025-04-20 23:31:41 -0400

    docs

--- src/validation/format.ts
+++ src/validation/format.ts
@@ -1,8 +1,26 @@
+/**
+ * Defines a message formatter.
+ */
 export interface MessageFormatter {
+  /**
+   * Formats a message with the given placeholders.
+   * @param message The message to format.
+   * @param placeholders The placeholders to replace in the message.
+   * @returns The formatted message.
+   */
   format(message: string, placeholders: Record<string, unknown>): string;
 }
 
+/**
+ * The default message formatter. This could use [mustache.js](https://github.com/janl/mustache.js), but we don't want to add a dependency for this. We simply replace occurrences of placeholder keys with their values, no other computation.
+ */
 export default class DefaultMessageFormatter implements MessageFormatter {
+  /**
+   * Formats a message with the given placeholders.
+   * @param message The message to format.
+   * @param placeholders The placeholders to replace in the message.
+   * @returns The formatted message.
+   */
   format(message: string, placeholders: Record<string, unknown>): string {
     let formatted: string = message;
     for (const key in placeholders) {
